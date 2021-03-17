c07455dcbf87ba305def8145b5747b6736d7f8d5
tohzen <melvin.l.mobley@gmail.com>
2021-03-17 11:58:33 -0400

    done

--- script.js
+++ script.js
@@ -15,10 +15,18 @@ const computerPaddle = document.querySelector('.computer-paddle');
 // Initial computer paddle y-position and y-velocity
 let computerPaddleYPosition = 0;
 let computerPaddleYVelocity = 1;
+// ==========Ball====================================
+const moveball = document.querySelector('#uwu');
+let ballPositionX = GAME_AREA_WIDTH/2;
+let ballPositionY = GAME_AREA_HEIGHT/2;
+let ballVelo = 1;
+let ballRadius = 20;
+let dy = -1;
+let dx = -1;
 
 // Update the pong world
 function update() {
-
+// ====================paddles=======================
     // Update the computer paddle's position
     computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
 
@@ -27,6 +35,21 @@ function update() {
 
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
+
+    // ================ball=========================
+    
+    // ballPositionX++;
+    // ballPositionY++;
+    ballPositionY = ballPositionY + dy;
+    ballPositionX = ballPositionX + dx;
+    if(ballPositionY >= GAME_AREA_HEIGHT-BALL_SIZE || ballPositionY < 0 ){
+        dy = ballPositionY * dy;
+    }
+    if(ballPositionX > GAME_AREA_WIDTH-BALL_SIZE-BALL_SIZE || ballPositionX  < 0 ){
+        dx = ballPositionX * dx;
+    }
+    moveball.style.top = `${ballPositionY}px`;
+    moveball.style.left = `${ballPositionX}px`;
 }
 
 // Call the update() function everytime the browser is ready to re-render
